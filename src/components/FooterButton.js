1dfedf038f4c66f93a6a87cc1acf7b9a2b74a459
Le Tan Thang <letanthang@gmail.com>
2017-09-26 10:06:36 +0700

    add new icon

--- src/components/FooterButton.js
+++ src/components/FooterButton.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { Button, Text } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Colors } from '../Styles';
 
-const FooterButton = ({ active, text, normalIcon, activeIcon, flip, onPress }) => {
+const FooterButton = ({ active, text, normalImage, activeImage, normalIcon, activeIcon, flip, onPress }) => {
   const onButtonPress = active ? null : onPress;
   let color = Colors.footerNormal;
   let iconName = normalIcon;
   let style = null;
+  let imageSource = normalImage;
   if (flip) {
     style = { transform: [{ rotateY: '180deg' }] };
   }
   if (active) {
     color = Colors.footerActive;
     if (activeIcon) iconName = activeIcon;
+    if (activeImage) imageSource = activeImage;
+  }
+  if (!imageSource) {
+    return (
+      <Button
+        onPress={onButtonPress}
+        transparent
+      >
+        <Icon name={iconName} size={32} color={color} style={style} />
+        <Text style={{ color }}>{text}</Text>
+      </Button>
+    );
   }
   return (
     <Button
       onPress={onButtonPress}
       transparent
     >
-      <Icon name={iconName} size={32} color={color} style={style} />
+      <Image source={imageSource} style={{ width: 25, height: 25, margin: 5 }} />
       <Text style={{ color }}>{text}</Text>
     </Button>
   );

26f26b87d94027e3610e571e71bcf6916ed3b073
Le Tan Thang <ghn.tech.thanglt@gmail.com>
2017-10-05 10:15:49 +0700

    temp way to disable log

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { View } from 'react-native';
 import { Root, StyleProvider } from 'native-base';
 import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'remote-redux-devtools';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
 import configureStore from './configureStore';
 import LoginScreen from './screens/LoginScreen';
 import Drawer from './Drawer';
@@ -25,6 +21,15 @@ import TripListScreen from './screens/TripListScreen';
 import getTheme from '../native-base-theme/components';
 import material from '../native-base-theme/variables/material';
 import platform from '../native-base-theme/variables/platform';
+
+if (false || (process.env.NODE_ENV || '').toLowerCase() === 'production') {
+  // disable console. log in production
+  console.log = function () {};
+  console.info = function () {};
+  console.warn = function () {};
+  console.error = function () {};
+  console.debug = function () {};
+}
 
 //export const store = configureStore();
 class App extends Component {

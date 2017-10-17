5ee9af07d46ce7d3a82f7f3f657269a6dfe5ecc6
Le Tan Thang <letanthang@gmail.com>
2017-10-17 17:07:38 +0700

    unlink uninstall realm

--- android/app/src/main/java/com/mpds_new/MainApplication.java
+++ android/app/src/main/java/com/mpds_new/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.reactnativecallhistory.RNCallHistoryPackage;
-import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new RCTCameraPackage(),
             new RNCallHistoryPackage(),
-            new RealmReactPackage(),
             new VectorIconsPackage()
       );
     }

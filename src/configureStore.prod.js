8ccfd6350c0c49e15fc9591ac8354f71340a487e
Le Tan Thang <letanthang@gmail.com>
2018-01-08 17:08:39 +0700

    redux persist

--- src/configureStore.prod.js
+++ src/configureStore.prod.js
@@ -2,8 +2,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export default function configureStore() {
-  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-  return store;
+  const config = {
+    key: 'root',
+    storage
+  };
+  const reducer = persistCombineReducers(config, reducers);
+  const store = createStore(reducer, {}, applyMiddleware(ReduxThunk));
+  const persistor = persistStore(store);
+  return { store, persistor };
 }

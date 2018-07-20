d10fbb1827afa3ec6383a7c2675167aa9767f51a
Le Tan Thang <letanthang@gmail.com>
2018-07-20 13:25:48 +0700

    change to staging

--- src/apis/MPDS.js
+++ src/apis/MPDS.js
@@ -20,19 +20,19 @@ export const appVersionName = '11/07';
 // const INSIDE_URL = 'http://api.insidev2.ghn.vn/sorting/v1';
 // export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/sso-login';
 
-// const PDS_URL = 'http://api.staging.lastmile.ghn.vn/lastmile/v1';
-// const ACC_URL = 'http://api.staging.lastmile.ghn.vn/acc/v1';
-// const OSS_URL = 'http://api.staging.ops.ghn.vn/oss/v2';
-// const LOG_URL = 'http://api.staging.ops.ghn.vn/als/v1';
-// const INSIDE_URL = 'http://api.insidev2.ghn.vn/sorting/v1';
-// export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/sso-login';
-
-const PDS_URL = 'http://api.dev.lastmile.ghn.vn/lastmile/v1';
-const ACC_URL = 'http://api.dev.lastmile.ghn.vn/acc/v1';
+const PDS_URL = 'http://api.staging.lastmile.ghn.vn/lastmile/v1';
+const ACC_URL = 'http://api.staging.lastmile.ghn.vn/acc/v1';
 const OSS_URL = 'http://api.staging.ops.ghn.vn/oss/v2';
 const LOG_URL = 'http://api.staging.ops.ghn.vn/als/v1';
 const INSIDE_URL = 'http://api.insidev2.ghn.vn/sorting/v1';
 export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/sso-login';
+
+// const PDS_URL = 'http://api.dev.lastmile.ghn.vn/lastmile/v1';
+// const ACC_URL = 'http://api.dev.lastmile.ghn.vn/acc/v1';
+// const OSS_URL = 'http://api.staging.ops.ghn.vn/oss/v2';
+// const LOG_URL = 'http://api.staging.ops.ghn.vn/als/v1';
+// const INSIDE_URL = 'http://api.insidev2.ghn.vn/sorting/v1';
+// export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/sso-login';
 
 const Share = new ShareVariables();
 const mock = mockOn ? new MockAdapter(axios) : null;

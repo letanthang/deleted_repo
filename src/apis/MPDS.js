223129d4e456e3c203e0e45c250e4fb3fab7f8da
Nguyễn Chính Thuận <tcn@TCN-macbook.local>
2018-11-14 18:36:12 +0700

    update_production

--- src/apis/MPDS.js
+++ src/apis/MPDS.js
@@ -11,8 +11,8 @@ import { infoResponse, loginResponse, addOrdersResponse, orderDetailResponse, or
 // ---------turn on mock data----------
 const mockOn = false;
 const timeout = 9500;
-export const live = false;
-export const appVersionName = '12/10';
+export const live = true;
+export const appVersionName = '15/11';
 
 // const PDS_URL = 'http://api.lastmile.ghn.vn/trip/v2';
 // const ACC_URL = 'http://api.lastmile.ghn.vn/acc/v1';
@@ -25,25 +25,25 @@ export const appVersionName = '12/10';
 // const ApiSecret = 'df6f564cGJRf9fGF6CPWJSqslvhaaaqqYafjfnQC3DfjQdbc47';
 
 
-// const PDS_URL = 'http://api.uat.lastmile.ghn.vn/trip/v2';
-// const ACC_URL = 'http://api.uat.lastmile.ghn.vn/acc/v1';
-// const OSS_URL = 'http://api.uat.ops.ghn.vn/oss/v2';
-// const OMS_URL = 'http://api.uat.ops.ghn.vn/oms/v1';
-// const LOG_URL = 'http://api.uat.ops.ghn.vn/als/v1';
-// const INSIDE_URL = 'http://api.uat.insidev2.ghn.vn/sorting/v1';
+const PDS_URL = 'http://api.uat.lastmile.ghn.vn/trip/v2';
+const ACC_URL = 'http://api.uat.lastmile.ghn.vn/acc/v1';
+const OSS_URL = 'http://api.uat.ops.ghn.vn/oss/v2';
+const OMS_URL = 'http://api.uat.ops.ghn.vn/oms/v1';
+const LOG_URL = 'http://api.uat.ops.ghn.vn/als/v1';
+const INSIDE_URL = 'http://api.uat.insidev2.ghn.vn/sorting/v1';
+export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/hms/static';
+const ApiKey = 'TEST@APIKEY';
+const ApiSecret = 'df6f564cGJRf9fGF6CPWJSqslvhaaaqqYafjfnQC3DfjQdbc47';
+
+// const PDS_URL = 'http://api.staging.lastmile.ghn.vn/trip/v2';
+// const ACC_URL = 'http://api.staging.lastmile.ghn.vn/acc/v1';
+// const OSS_URL = 'http://api.staging.ops.ghn.vn/oss/v2';
+// const OMS_URL = 'http://api.staging.ops.ghn.vn/oms/v1';
+// const LOG_URL = 'http://api.staging.ops.ghn.vn/als/v1';
+// const INSIDE_URL = 'http://api.staging.insidev2.ghn.vn/sorting/v1';
 // export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/hms/static';
 // const ApiKey = 'TEST@APIKEY';
-// const ApiSecret = 'df6f564cGJRf9fGF6CPWJSqslvhaaaqqYafjfnQC3DfjQdbc47';
-
-const PDS_URL = 'http://api.staging.lastmile.ghn.vn/trip/v2';
-const ACC_URL = 'http://api.staging.lastmile.ghn.vn/acc/v1';
-const OSS_URL = 'http://api.staging.ops.ghn.vn/oss/v2';
-const OMS_URL = 'http://api.staging.ops.ghn.vn/oms/v1';
-const LOG_URL = 'http://api.staging.ops.ghn.vn/als/v1';
-const INSIDE_URL = 'http://api.staging.insidev2.ghn.vn/sorting/v1';
-// export const authenUri = 'https://hr.ghn.vn/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/hms/static';
-const ApiKey = 'TEST@APIKEY';
-const ApiSecret = 'df6f564cGJRf9fGF6C9cLRyzjp8mpYafjfnQC3DfjQdbc47';
+// const ApiSecret = 'df6f564cGJRf9fGF6C9cLRyzjp8mpYafjfnQC3DfjQdbc47';
 
 
 // const PDS_URL = 'http://api.dev.lastmile.ghn.vn/trip/v2';
@@ -52,7 +52,7 @@ const ApiSecret = 'df6f564cGJRf9fGF6C9cLRyzjp8mpYafjfnQC3DfjQdbc47';
 // const OMS_URL = 'http://api.staging.ops.ghn.vn/oms/v1';
 // const LOG_URL = 'http://api.staging.ops.ghn.vn/als/v1';
 // const INSIDE_URL = 'http://api.insidev2.ghn.vn/sorting/v1';
-export const authenUri = 'http://103.20.148.181:8099/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/hms/static';
+// export const authenUri = 'http://103.20.148.181:8099/Home/Login?AppKey=BB17y1A9A0128b7677C940784CE11A28DE2B3&returnUrl=http://lastmile.ghn.vn/hms/static';
 // const ApiKey = 'TEST@APIKEY';
 // const ApiSecret = 'df6f564cGJRf9fGF6C9cLRyzjp8mpYafjfnQC3DfjQdbc47';
 
